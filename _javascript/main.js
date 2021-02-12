5f02b9351430bdc789f28db340fddab3f0ab918d
Arthur <arthur.dennetiere@gmail.com>
2021-02-12 16:50:09 +0100

    Add js + firebase

--- _javascript/main.js
+++ _javascript/main.js
@@ -1,3 +1,9 @@
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('Hello Bulma!');
-});
+//Open modal
+const addProductButton = document.getElementById('add-product');
+const closeAddProduct = document.getElementById('close-addProduct');
+[addProductButton, closeAddProduct].forEach(button => button.onclick = () => openCloseModal() )
+  function openCloseModal(){
+      let modal = document.getElementById("addProduct");
+      modal.classList.toggle('is-active');
+}
+
